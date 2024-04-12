7ac4ab39a4da6c087105adc26f8ad823d7d2a756
Duc Anh <bully0707ducanh@gmail.com>
2024-04-13 00:45:32 +0700

    OKAY ITS DONE BRO

--- index.js
+++ index.js
@@ -21,14 +21,14 @@ window.onload = () => {
                 box1.innerHTML = 'X';
                 turn = 1;
 
-                iswin(i % 36,Math.floor(i/36));
+                
             } else if (turn == 1) {
                 box1.innerHTML = 'O';
                 turn = 0;
 
-                iswin(i % 36,Math.floor(i/36));
+                
             }
-
+            iswin(Math.floor(i/36),i % 36);
             // boardItem.push(att2.value)
             // console.log(boardItem)
         });
@@ -37,6 +37,7 @@ window.onload = () => {
         // box1.innerHTML = "⠀"
 
         board.appendChild(box1);
+        
     }
 };
 var layout = [];
@@ -54,6 +55,8 @@ function getBoard() {
     return layout;
 }
 function iswin(x,y) {
+    console.log(getBoard())
+    console.log(String(x)+':'+String(y))
     const countAxes = {
         horizontal: 1,
         vertical: 1,
@@ -70,6 +73,34 @@ function iswin(x,y) {
         [-1, 1, 'diagonal2'],
         [1, -1, 'diagonal2'],
     ];
+    let cur_x = x
+    let cur_y = y
+    for (let o of dirs){
+        x = cur_x
+        y = cur_y
+        console.log(o[2])
+        for (let z = 0;z<5;z++){
+            x+=o[0]
+            y+=o[1] 
+
+            console.log(getBoard()[cur_x][cur_y])
+            if (getBoard()[cur_x][cur_y]==getBoard()[x][y]){
+                
+                countAxes[o[2]]+=1
+                console.log(countAxes)
+                if (countAxes[o[2]]==5){
+                    const scoreBoard = document.getElementById("scoreBoard")
+                    const board = document.getElementById("boardHandle")
+                    scoreBoard.innerHTML=`player ${getBoard()[cur_x][cur_y]} win`
+                    board.innerHTML=""
+                    
+                }
+            } else {
+                break
+            }
+        }
+    }
+
     // const char =
     //check moi huong cho den khi gap vat can luu lai diem doi huong nguoc lai
 }
